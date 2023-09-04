f60ad7f283ea6112950d1500ed66a285fc598265
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-09-04 02:07:53 +0100

    Card ongoing

--- src/Components/Dashboard/MyVideos.jsx
+++ src/Components/Dashboard/MyVideos.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
 import { TERipple } from 'tw-elements-react';
 
 const MyVideos = () => {
   return (
-    <div className="ml-auto rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    <div className="w-64 ml-64 border border-neutral-300 rounded-lg dark:bg-neutral-700">
       <TERipple>
-        <div className="relative overflow-hidden bg-cover bg-no-repeat">
-          <img className="rounded-t-lg" width={200} height={200} src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg" alt="" />
-          <a href="#!">
-            <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-          </a>
+        <div className="bg-cover bg-no-repeat">
+          <img className="rounded-t-lg" width={400} height={400} src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg" alt="" />
         </div>
       </TERipple>
-      <div className="p-4"> {/* Reduced padding */}
+      <div> {/* Reduced padding */}
+      <span className="rounded-full text-2xl bg-white text-black ">KA</span>
+      <span className="text-2xl ml-2 text-white ">User</span>
+      <span className="text-sm ml-2 text-white ">9 days</span>
+
+
         <h5 className="mb-1 text-lg font-medium leading-tight text-neutral-800 dark:text-neutral-50"> {/* Reduced font size */}
           Card title
         </h5>
