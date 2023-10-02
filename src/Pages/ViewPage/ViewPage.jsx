621deeb2ed9fd76725f1460b3e9253715c324a79
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-10-03 00:30:07 +0100

    prompt navigation notice done

--- src/Pages/ViewPage/ViewPage.jsx
+++ src/Pages/ViewPage/ViewPage.jsx
@@ -1,15 +1,33 @@
-import NavBar from '../../Components/Homepage/NavBar'
-import ViewSection from '../../Components/ViewSection/ViewSection'
+import { useState, useEffect } from 'react';
+import NavBar from '../../Components/Homepage/NavBar';
+import ViewSection from '../../Components/ViewSection/ViewSection';
 
 const ViewPage = () => {
-  return (
-    <div  className='bg-gray-50 dark:bg-gray-800'>
-    <div>
-        <NavBar />
-      <ViewSection />
-    </div>
-    </div>
-  )
-}
 
-export default ViewPage
+  const handleLeave = (event) => {
+    const message = 'Are you sure you want to leave?';
+    (event || window.event).returnValue = message; // Standard for most browsers
+    return message; // For some older browsers
+  };
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleLeave);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleLeave);
+    };
+  }, []);
+
+
+
+  return (
+    <div className='bg-gray-50 dark:bg-gray-800'>
+      <div>
+        <NavBar />
+        <ViewSection />
+      </div>
+    </div>
+  );
+};
+
+export default ViewPage;
