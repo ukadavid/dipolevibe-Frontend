c2e070db275923b56abdf168ef9dcbc0b6c94d4a
wakil <wakilsalaudeen@gmail.com>
2023-10-10 16:40:07 +0100

    update VideoCard Ui, add SearchBar

--- src/Components/VideoList/VideoListPage.jsx
+++ src/Components/VideoList/VideoListPage.jsx
@@ -2,6 +2,7 @@ import { apiGetVideos } from '../../Context/Api/Axios';
 import React, { useEffect, useState } from 'react';
 import VideoCard from './VideoCard';
 import Preloader from '../Preloader/Preloader';
+import SearchBar from './SearchBar';
 
 function VideoList() {
   const [videos, setVideos] = useState([]);
@@ -41,12 +42,16 @@ function VideoList() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8 my-14">
+           <SearchBar onSearch={ setVideos }/>
         <div className="grid grid-cols-2 gap-4">
           {videos.map((video, index) => (
             <VideoCard key={index} video={video} index={index} />
           ))}
         </div>
-        <button onClick={loadMore}>Load More</button>
+        <div  className="flex justify-center">
+          <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 w-40" 
+           onClick={loadMore}>Load More</button>
+        </div>
       </div>
     </>
   );
