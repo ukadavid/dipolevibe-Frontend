ee5e7a048006c3a7ba0d7b75f803154d79e5235b
Oluwatobi Ishola <ishoshot@gmail.com>
2023-11-09 18:57:45 +0100

    Update the NavBar and Hero Section

--- src/Components/Homepage/Header.jsx
+++ src/Components/Homepage/Header.jsx
@@ -1,69 +1,98 @@
 import HeaderPicture from "../../assets/pictureHeader.png";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { RecorderComponent } from "../RecorderModals/RecorderModal";
+import { Link } from "react-router-dom";
 
 import "./css/animate.css";
+
 const Header = () => {
-  const [ref, inView] = useInView({triggerOnce: true})
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  const [isScreenModalOpen, setScreenModalOpen] = useState(false);
+
+  const openScreenModal = () => {
+    setScreenModalOpen(true);
+  };
+
+  const closeScreenModal = () => {
+    setScreenModalOpen(false);
+  };
   return (
     <>
-      <section className="bg-gray-800 dark:bg-gray-900 ">
-        <div className="grid max-w-screen-xl px-4 pt-24 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-32 lg:grid-cols-12 lg:pt-28">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <motion.h1
-            ref={ref}
-            initial={{opacity: 0, y: 50}}
-            animate={{opacity: inView ? 1 : 0, y: inView ? 0 : 20}}
-            transition={{delay: 0.1, duration: 0.5}}
-            className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl text-white xl:text-6xl dark:text-white">
-              One video is worth
-              <br />a thousand words
-            </motion.h1>
-            <motion.p 
-            ref={ref}
-            initial={{opacity: 0, y: 50}}
-            animate={{opacity: inView ? 1 : 0, y: inView ? 0 : 20}}
-            transition={{delay: 0.2, duration: 0.5}}
-            className="max-w-xs lg:max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-500 dark:text-gray-400">
-              DipoVibe empowers you to capture video, audio, and screen
-              recordings effortlessly, allowing seamless sharing. Our advanced
-              transcription feature eliminates distractions, enabling you to
-              focus and work more efficiently.
-            </motion.p>
+      <div className="relative overflow-hidden bg-white">
+        <div className="pt-40 mx-auto xl:gap-0 lg:pt-50 lg:pb-40">
+          <div className="relative items-center justify-between lg:flex lg:px-20">
+            <div className="mt-4xl w-full lg:w-0.9/2">
+              <div className="flex justify-center mb-8 lg:justify-start">
+                <div className="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  Want to catch up with our story?
+                  <Link
+                    href="#Stories"
+                    className="mx-1 font-semibold text-blue-500"
+                  >
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    Read more
+                    <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                </div>
+              </div>
+              <div className="text-center lg:text-left">
+                <motion.h1
+                  ref={ref}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+                  transition={{ delay: 0.1, duration: 0.5 }}
+                  className="text-4xl font-semibold leading-10 tracking-tight text-gray-900 lg:text-5xl"
+                >
+                  Record your next big thing
+                </motion.h1>
 
-            <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-              <motion.a
-              ref={ref}
-              initial={{opacity: 0, y: 50}}
-              animate={{opacity: inView ? 1 : 0, y: inView ? 0 : 20}}
-              transition={{delay: 0.3, duration: 0.5}}
-                href="#"
-                className="inline-flex items-center justify-center text-white w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-full sm:w-auto hover:text-black hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 hover:animate-bounce"
-              >
-                dipoVibe for free
-              </motion.a>
+                <motion.p
+                  ref={ref}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+                  transition={{ delay: 0.3, duration: 0.5 }}
+                  className="px-5 mt-6 leading-8 text-gray-600 lg:px-0"
+                >
+                  DipoVibe empowers you to capture video, audio, and screen
+                  recordings effortlessly, allowing seamless sharing. <br /> Our
+                  advanced transcription feature eliminates distractions,
+                  enabling you to focus and work more efficiently.
+                </motion.p>
+
+                <div className="flex items-center justify-center mt-10 lg:justify-start gap-x-6">
+                  <motion.p
+                    ref={ref}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+                    transition={{ delay: 0.5, duration: 0.5 }}
+                    href="#"
+                    onClick={openScreenModal}
+                    className="flex items-center gap-2 px-5 py-3 text-sm font-semibold text-white bg-blue-500 rounded-md shadow-sm cursor-pointer"
+                  >
+                    Record video for free
+                  </motion.p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center w-full lg:w-1/2">
+              <img
+                src={HeaderPicture}
+                alt="hero image"
+                className="w-full h-auto mt-10 mt-7xl lg:mt--10"
+              />
             </div>
           </div>
-          <div className="lg:mt-0 lg:col-span-5 lg:flex mt-8">
-            <motion.div
-              initial={{
-                rotate: "0deg",
-                scale: 0,
-              }}
-              animate={{
-                rotate: "360deg",
-                scale: 1,
-              }}
-              transition={{
-                duration: 1,
-                ease: "easeInOut",
-              }}
-            >
-              <img src={HeaderPicture} alt="hero image" />
-            </motion.div>
-          </div>
         </div>
-      </section>
+      </div>
+
+      {/* Modal */}
+      {isScreenModalOpen && (
+        <RecorderComponent closeScreenModal={closeScreenModal} />
+      )}
     </>
   );
 };
--- src/Components/Homepage/NavBar.jsx
+++ src/Components/Homepage/NavBar.jsx
@@ -2,23 +2,14 @@ import { useState } from "react";
 import { FaDesktop } from "react-icons/fa";
 import { RecorderComponent } from "../RecorderModals/RecorderModal";
 import ThemeToggle from "../DashboardUpdate/navComponent/ThemeToggle";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const [isScreenModalOpen, setScreenModalOpen] = useState(false);
-
   // toggle menu section
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
-  };
-
-  const openScreenModal = () => {
-    setScreenModalOpen(true);
-  };
-
-  const closeScreenModal = () => {
-    setScreenModalOpen(false);
   };
 
   return (
@@ -26,92 +17,78 @@ const NavBar = () => {
       <div>
         <header className="relative z-10">
           <nav
-            className="fixed bg-white bg-opacity-90 top-0 left-0 right-0 z-10 flex items-center justify-between p-6 lg:px-8"
+            className="fixed top-0 left-0 right-0 px-8 py-5 bg-white shadow lg:py-8 lg:px-12"
             aria-label="Global"
           >
-            <div className="flex lg:flex-1">
-              <a href="/" className="-m-1.5 p-1.5">
-                <span className="text-right font-medium text-sm text-black py-1 px-3 w-8 h-8 flex items-center justify-center bg-slate-700 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-600/80 rounded-full">
-                  Dip <span className="text-white mx-0.5">olev</span>ibe
-                </span>
-              </a>
-            </div>
-            <div className="flex lg:hidden">
-              <button
-                type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                onClick={toggleMobileMenu}
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className={`h-6 w-6 ${mobileMenuOpen ? "hidden" : "block"}`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-                <svg
-                  className={`h-6 w-6 ${mobileMenuOpen ? "block" : "hidden"}`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="hidden lg:flex lg:gap-x-12">
-              <a
-                href="/"
-                className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
-              >
-                Home
-              </a>
-              <a
-                href="/signup"
-                className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
-              >
-                Register
-              </a>
-              <a
-                href="/login"
-                className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
-              >
-                Login
-              </a>
-              <a
-                href="/videolist"
-                className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
-              >
-                Video Library
-              </a>
-            </div>
-            <div className="hidden lg:flex items-center lg:flex-1 lg:justify-end">
-              <div className="mr-4">
-                <ThemeToggle />
+            <div className="flex items-center justify-between">
+              {/*  */}
+              <div className="flex lg:flex-1">
+                <Link className="ml-10" to="/">
+                  <span className="flex items-center justify-center w-8 h-8 px-3 py-1 text-sm font-medium text-right text-black rounded-full bg-slate-700 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-600/80">
+                    Dip <span className="text-white mx-0.5">olev</span>ibe
+                  </span>
+                </Link>
               </div>
 
-              <div>
-                <a
-                  href="#"
-                  className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
-                  onClick={openScreenModal}
+              {/*  */}
+              <div className="flex lg:hidden">
+                <button
+                  type="button"
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                  onClick={toggleMobileMenu}
                 >
-                  <FaDesktop /> <span aria-hidden="true"></span>
-                </a>
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    className={`h-6 w-6 ${mobileMenuOpen ? "hidden" : "block"}`}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                  <svg
+                    className={`h-6 w-6 ${mobileMenuOpen ? "block" : "hidden"}`}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              {/*  */}
+              <div className="items-center hidden text-sm lg:flex lg:gap-x-12 lg:flex-2 lg:justify-end ">
+                <Link className="text-gray-900 hover:text-gray-600" to="/">
+                  Home
+                </Link>
+                <Link className="text-gray-900 hover:text-gray-600" to="/">
+                  Features
+                </Link>
+                <Link className="text-gray-900 hover:text-gray-600" to="/">
+                  Pricing
+                </Link>
+                <Link
+                  className="text-gray-900 hover:text-gray-600"
+                  to="/videolist"
+                >
+                  Video Stash
+                </Link>
+                <Link className="text-white bg-blue-500 btn" to="/signup">
+                  Get started
+                </Link>
               </div>
             </div>
           </nav>
@@ -120,7 +97,7 @@ const NavBar = () => {
           {mobileMenuOpen && (
             <div className="lg:hidden" role="dialog" aria-modal="true">
               <div className="fixed inset-0 z-10"></div>
-              <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="fixed inset-y-0 right-0 z-50 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
                   <a href="#" className="-m-1.5 p-1.5">
                     <span className="sr-only">dipoleVibe</span>
@@ -132,7 +109,7 @@ const NavBar = () => {
                   >
                     <span className="sr-only">Close menu</span>
                     <svg
-                      className="h-6 w-6"
+                      className="w-6 h-6"
                       fill="none"
                       viewBox="0 0 24 24"
                       strokeWidth="1.5"
@@ -147,35 +124,35 @@ const NavBar = () => {
                     </svg>
                   </button>
                 </div>
-                <div className="mt-6 flow-root">
+                <div className="flow-root mt-6">
                   <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
+                    <div className="py-6 space-y-2">
                       <a
                         href="/"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                       >
                         Home
                       </a>
                       <a
                         href="/signup"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                       >
                         Register
                       </a>
                       <a
                         href="/login"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                       >
                         Login
                       </a>
                       <a
                         href="/videolist"
-                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
                       >
                         Video Library
                       </a>
                     </div>
-                    <div className="py-6 flex-col items-center">
+                    <div className="flex-col items-center py-6">
                       <div>
                         <ThemeToggle />
                       </div>
@@ -183,7 +160,7 @@ const NavBar = () => {
                       <div className="mt-4 ml-2">
                         <a
                           href="#"
-                          className="text-sm hover:text-blue-500 transition font-semibold leading-6 text-gray-900"
+                          className="text-sm font-semibold leading-6 text-gray-900 transition hover:text-blue-500"
                           onClick={openScreenModal}
                         >
                           <FaDesktop /> <span aria-hidden="true"></span>
@@ -197,10 +174,6 @@ const NavBar = () => {
           )}
         </header>
       </div>
-      {/* Modal */}
-      {isScreenModalOpen && (
-        <RecorderComponent closeScreenModal={closeScreenModal} />
-      )}
     </>
   );
 };
